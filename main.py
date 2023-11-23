d39352c6d5db4e405ef5a0beeb36ecb52c1dfb14
kosei <abdelhamid.boudjit@gmail.com>
2023-11-23 18:29:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from utils.download_dataset import download_dataset
+from preprocessing import train_test_split
 
 
 def main():
     download_dataset()
+    train_test_split()
 
 
 if __name__ == "__main__":
