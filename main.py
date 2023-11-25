125abb11622de972238336b4b77988e0e7f3e4ca
kosei <abdelhamid.boudjit@gmail.com>
2023-11-25 17:21:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils.download_dataset import download_dataset
 from preprocessing.train_test_split import train_test_split
 from models.image_classification_V0 import ImageModelV0
 from preprocessing.transform import transforms
+from eval.single_image_eval import eval
 
 torch.manual_seed(42)
 
@@ -13,7 +14,7 @@ def main():
     class_names, class_dict, test_dataloader, train_dataloader, train_data, test_data = transforms
     model_0 = ImageModelV0(input_shape=3, hidden_units=128,
                            output_shape=len(class_names))
-    print(model_0)
+    eval(data_loader=train_dataloader, model=model_0)
 
 
 if __name__ == "__main__":
